033e22d8643f983d079febe50851409e553030c7
Shogo Akimoto <114.s.aki@gmail.com>
2019-11-30 21:42:40 +0900

    refactor: resolve lint error

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,15 +1,12 @@
 import React from "react"
 import galleryInfoList from "../shared/const/galleryInfoList"
-console.log(galleryInfoList)
 
 const Top = () => {
   return (
     <div>
       {galleryInfoList.map(galleryInfo => {
         const { thumbPath } = galleryInfo
-        return (
-          <img key={thumbPath} src={`/images/gallery${thumbPath}`} alt=""/>
-        )
+        return <img key={thumbPath} src={`/images/gallery${thumbPath}`} alt="" />
       })}
     </div>
   )
--- src/client/shared/const/galleryInfoList.ts
+++ src/client/shared/const/galleryInfoList.ts
@@ -1,1 +1,20 @@
-export default [{"path":"/05.Akita/2018/_DSC0310.webp.jpg","thumbPath":"/05.Akita/2018/_DSC0310.thumb.webp.jpg","year":2018,"prefCode":"05"},{"path":"/01.Hokkaido/2017/_DSC9344.webp.jpg","thumbPath":"/01.Hokkaido/2017/_DSC9344.thumb.webp.jpg","year":2017,"prefCode":"01"},{"path":"/01.Hokkaido/2017/_DSC9313.webp.jpg","thumbPath":"/01.Hokkaido/2017/_DSC9313.thumb.webp.jpg","year":2017,"prefCode":"01"}]
+export default [
+  {
+    path: "/05.Akita/2018/_DSC0310.webp.jpg",
+    thumbPath: "/05.Akita/2018/_DSC0310.thumb.webp.jpg",
+    year: 2018,
+    prefCode: "05",
+  },
+  {
+    path: "/01.Hokkaido/2017/_DSC9344.webp.jpg",
+    thumbPath: "/01.Hokkaido/2017/_DSC9344.thumb.webp.jpg",
+    year: 2017,
+    prefCode: "01",
+  },
+  {
+    path: "/01.Hokkaido/2017/_DSC9313.webp.jpg",
+    thumbPath: "/01.Hokkaido/2017/_DSC9313.thumb.webp.jpg",
+    year: 2017,
+    prefCode: "01",
+  },
+]
