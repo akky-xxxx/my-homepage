437c069e2835d6db00a070dfed261d1ca5f354ce
Shogo Akimoto <114.s.aki@gmail.com>
2019-12-01 18:41:52 +0900

    feat: implement bff

---
+++ src/server/serverMiddleWares/replaceToWebp.ts
@@ -0,0 +1,19 @@
+/**
+ * import node_modules
+ */
+import { Request, Response, NextFunction } from "express-serve-static-core"
+
+/**
+ * webp が使える環境で ファイル名にjpg, png を webp に差し替える
+ */
+const replaceToWebpMiddleWare = (req: Request, _res: Response, next: NextFunction) => {
+  if (
+    req.headers.accept &&
+    req.headers.accept.includes("image/webp")
+  ) {
+    req.url = req.url.replace(".webp.", ".webp.")
+  }
+  next()
+}
+
+export default replaceToWebpMiddleWare
---
+++ src/server/www.ts
@@ -0,0 +1,49 @@
+/**
+ * import node_modules
+ */
+import next from "next"
+import express from "express"
+import path from "path"
+import chalk from "chalk"
+
+import replaceToWebpMiddleWare from "./serverMiddleWares/replaceToWebp"
+
+/**
+ * import others
+ */
+import nextConfig from "../../next.config"
+
+
+/**
+ * main
+ */
+const PORT = 3000
+const isDev = process.env.NODE_ENV !== "production"
+const ROOT_DIR = path.resolve(__dirname, "../")
+const CLIENT_DIR = path.resolve(ROOT_DIR, "src/client")
+const app = next({
+  dir: CLIENT_DIR,
+  conf: nextConfig,
+  dev: isDev,
+})
+const handle = app.getRequestHandler()
+
+app
+  .prepare()
+  .then(() => {
+    const server = express()
+
+    server.get("*.webp.*", replaceToWebpMiddleWare)
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(PORT, (err: Error) => {
+      if (err) throw err
+      console.log(chalk.cyan("======================================================================================="))
+      console.log(`${chalk.cyan("■  ")}${chalk.green(`Ready on http://localhost:${PORT}`)}`)
+      console.log(`${chalk.cyan("■  ")}Serving files from: ${CLIENT_DIR}`)
+      console.log(chalk.cyan("======================================================================================="))
+    })
+  })
