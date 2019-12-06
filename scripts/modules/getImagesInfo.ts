ce85c4e250f543c6ec7b98f4c4e53d4e259eb2f7
Shogo Akimoto <114.s.aki@gmail.com>
2019-12-06 18:29:35 +0900

    feat: add property of photo gallery data

--- scripts/modules/getImagesInfo.ts
+++ scripts/modules/getImagesInfo.ts
@@ -1,20 +1,26 @@
 interface ImagesInfo {
+  imageId: number
   path: string
   thumbPath: string
   year: number
+  month: number
   prefCode: string
 }
 
 const imagesInfo = (fileNames: string[], ORIGIN_ROOT: string) =>
   fileNames
-    .reduce((arr: ImagesInfo[], path: string) => {
+    .reduce((arr: ImagesInfo[], path: string, index: number) => {
       const relatedPath = path.replace(ORIGIN_ROOT, "")
-      const [pref, year] = relatedPath.slice(1).split("/")
+      const [pref, yearMonth] = relatedPath.slice(1).split("/")
+      const year = yearMonth.slice(0, 4)
+      const month = yearMonth.slice(4)
       const prefCode = pref.slice(0, 2)
       arr.push({
+        imageId: index + 1,
         path: relatedPath,
         thumbPath: relatedPath.replace(".webp.jpg", ".thumb.webp.jpg"),
         year: parseInt(year, 10),
+        month: parseInt(month, 10),
         prefCode,
       })
       return arr
