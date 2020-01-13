31d7b2225869b89cfe1fdeee1612b9264fafdcef
Shogo Akimoto <114.s.aki@gmail.com>
2020-01-13 22:12:15 +0900

    feat: include nprogress

---
+++ src/client/assets/styles/vendors/index.ts
@@ -0,0 +1,16 @@
+/**
+ * import node_modules
+ */
+import { css } from "styled-components"
+
+/**
+ * import others
+ */
+import nprogress from "./nprogress"
+
+/**
+ * main
+ */
+export default css`
+  ${nprogress}
+`
