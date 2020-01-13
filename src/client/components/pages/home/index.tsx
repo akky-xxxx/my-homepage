d453cc36b6b5347edb8ab09cb8ca3cb32977119d
Shogo Akimoto <114.s.aki@gmail.com>
2020-01-13 15:46:30 +0900

    feat: implement dummy home

---
+++ src/client/components/pages/home/index.tsx
@@ -0,0 +1,14 @@
+/**
+ * import node_modules
+ */
+import React from "react"
+import { NextPage } from "next"
+
+/**
+ * main
+ */
+const Home: NextPage = () => {
+  return <div>Home</div>
+}
+
+export default Home
