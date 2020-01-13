efa6ec5f6fa3df97701412e7a0c099e3bf3a01d3
Shogo Akimoto <114.s.aki@gmail.com>
2020-01-13 14:47:57 +0900

    feat: common header and menu drawer

---
+++ src/client/components/layouts/layout/index.tsx
@@ -0,0 +1,33 @@
+/**
+ * import node_modules
+ */
+import React, { FC, Fragment, useState } from "react"
+
+/**
+ * import components
+ */
+import HeaderBar from "../headerBar"
+import MenuDrawer from "../menuDrawer"
+
+/**
+ * main
+ */
+const Layout: FC = props => {
+  const { children } = props
+  // TODO: redux に差し替える
+  const [isMenuOpen, setMenuOpen] = useState(false)
+
+  const handleOpenMenu = () => setMenuOpen(true)
+  const handleCloseMenu = () => setMenuOpen(false)
+
+  return (
+    <Fragment>
+      <HeaderBar handleOpenMenu={handleOpenMenu} />
+      <MenuDrawer isMenuOpen={isMenuOpen} handleCloseMenu={handleCloseMenu} />
+
+      {children}
+    </Fragment>
+  )
+}
+
+export default Layout
