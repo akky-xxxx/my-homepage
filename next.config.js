8ce636b03332150cb4f5754933312d192141ddee
Shogo Akimoto <114.s.aki@gmail.com>
2020-01-21 01:12:17 +0900

    chore: install @next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.NODE_ENV === "analyze",
+})
+
+module.exports = withBundleAnalyzer({
   distDir: "../../dist",
   webpack: (config, props) => {
     config.mode = props.dev ? "development" : "production"
 
     return config
   },
-}
+})
