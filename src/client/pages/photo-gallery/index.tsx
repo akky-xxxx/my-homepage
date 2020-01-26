78439da092a707f470141e5bf139ffd95b57c07c
Shogo Akimoto <114.s.aki@gmail.com>
2020-01-26 19:14:56 +0900

    feat: connect store and view

--- src/client/pages/photo-gallery/index.tsx
+++ src/client/pages/photo-gallery/index.tsx
@@ -19,7 +19,7 @@ import { HandleActions } from "../../store/modules/pages/photo-gallery/types"
  */
 const {
   pages: {
-    photoGallery: { openModal, closeModal, selectViewPref },
+    photoGallery: { openModal, closeModal, selectViewPref, selectViewTag },
   },
 } = actions
 
@@ -29,5 +29,6 @@ export default connect<{}, HandleActions, {}, State>(
     handleOpenModal: payload => dispatch(openModal(payload)),
     handleCloseModal: () => dispatch(closeModal()),
     handleSelectViewPref: payload => dispatch(selectViewPref(payload)),
+    handleSelectViewTag: payload => dispatch(selectViewTag(payload)),
   }),
 )(PhotoGallery)
--- src/client/store/modules/pages/photo-gallery/index.ts
+++ src/client/store/modules/pages/photo-gallery/index.ts
@@ -8,22 +8,26 @@ import { combineReducers } from "@reduxjs/toolkit"
  */
 import modalModule from "./modal"
 import viewPrefModule from "./viewPref"
+import viewTagsModule from "./viewTags"
 
 /**
  * main
  */
 const { actions: modalActions, reducer: modalReducer } = modalModule
 const { actions: viewPrefActions, reducer: viewPrefReducer } = viewPrefModule
+const { actions: viewTagsActions, reducer: viewTagsReducer } = viewTagsModule
 
 export const actions = {
   ...modalActions,
   ...viewPrefActions,
+  ...viewTagsActions,
 }
 
 // reducer
 const reducer = combineReducers({
   modal: modalReducer,
   selectedViewPref: viewPrefReducer,
+  selectedViewTags: viewTagsReducer,
 })
 
 export default reducer
