761bafee3c7ab3a72f32199f753460cb7342be80
Shogo Akimoto <114.s.aki@gmail.com>
2020-02-02 22:38:26 +0900

    refactor(createPhotoGalleryCell): add handling of re render

--- src/client/components/pages/photoGallery/createPhotoGalleryCell.tsx
+++ src/client/components/pages/photoGallery/createPhotoGalleryCell.tsx
@@ -1,8 +1,9 @@
 /**
  * import node_modules
  */
-import React, { FC } from "react"
+import React, { FC, memo } from "react"
 import { GridChildComponentProps } from "react-window"
+import { equals, pick } from "remeda"
 
 /**
  * import components
@@ -15,6 +16,7 @@ import GalleryCardWrapper from "./components/galleryCardWrapper"
 import { GalleryItem } from "../../../shared/types/pages/galleryList"
 import { HandleOpenModalAction } from "../../../store/modules/pages/photo-gallery/modal/types"
 import { HandleSelectViewTag } from "../../../store/modules/pages/photo-gallery/viewTags/types"
+import { IsNoReRender } from "../../../shared/types/common"
 
 /**
  * main
@@ -29,12 +31,26 @@ interface PhotoGalleryCellProps extends GridChildComponentProps {
   data: ExtendedGalleryItem[]
 }
 
+type GetIndex = (args: {
+  rowIndex: number
+  columnIndex: number
+  colNum: number
+}) => number
+
 export const galleryItemClassName = "galleryItemWrapper"
+const getIndex: GetIndex = args => {
+  const { columnIndex, rowIndex, colNum } = args
+  return rowIndex * colNum + columnIndex
+}
 
 const createPhotoGalleryCell = (colNum: number) => {
   const PhotoGalleryCell: FC<PhotoGalleryCellProps> = props => {
     const { style, rowIndex, columnIndex, data } = props
-    const index = rowIndex * colNum + columnIndex
+    const index = getIndex({
+      rowIndex,
+      columnIndex,
+      colNum,
+    })
     if (!data[index]) return null
     const { imageId, prefCode, path, date, tags, selectedViewTags, handleOpenModal, handleSelectViewTag } = data[index]
     const handleThisOpenModal = () => handleOpenModal({ targetId: imageId })
@@ -61,7 +77,24 @@ const createPhotoGalleryCell = (colNum: number) => {
     )
   }
 
-  return PhotoGalleryCell
+  const isNoReRender: IsNoReRender<PhotoGalleryCellProps> = (beforeProps, afterProps) => {
+    const beforeIndex = getIndex({
+      ...pick(beforeProps, ["rowIndex", "columnIndex"]),
+      colNum,
+    })
+    const afterIndex = getIndex({
+      ...pick(afterProps, ["rowIndex", "columnIndex"]),
+      colNum,
+    })
+
+    return (
+      beforeProps.style.top === afterProps.style.top &&
+      beforeProps.style.left === afterProps.style.left &&
+      equals(beforeProps.data[beforeIndex].selectedViewTags, afterProps.data[afterIndex].selectedViewTags)
+    )
+  }
+
+  return memo(PhotoGalleryCell, isNoReRender)
 }
 
 export default createPhotoGalleryCell
