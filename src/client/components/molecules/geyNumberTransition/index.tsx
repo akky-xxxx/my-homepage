1f687499baabbd725f93cab23eecdce4668007ea
Shogo Akimoto <114.s.aki@gmail.com>
2020-02-16 15:59:30 +0900

    feat(NumberTransition): implement NumberTransition
    https://github.com/akky-xxxx/my-homepage/issues/6

---
+++ src/client/components/molecules/geyNumberTransition/index.tsx
@@ -0,0 +1,50 @@
+/**
+ * import node_modules
+ */
+import React, { FC, memo, useState, Fragment } from "react"
+import gsap from "gsap"
+
+/**
+ * import other
+ */
+import { IsNoReRender } from "../../../shared/types/common"
+
+/**
+ * main
+ */
+interface NumberTransitionProps {
+  toNumber: number
+}
+
+const getNumberTransition = () => {
+  let beforeNumber = 0
+
+  const NumberTransition: FC<NumberTransitionProps> = props => {
+    const { toNumber } = props
+    const [currentNumber, setNumber] = useState(0)
+    const target = { count: beforeNumber }
+
+    React.useEffect(() => {
+      gsap.to(target, {
+        delay: 0.1,
+        duration: 0.5,
+        count: toNumber,
+        onUpdate: () => {
+          setNumber(Math.floor(target.count))
+        },
+        onComplete: () => {
+          beforeNumber = toNumber
+        },
+      })
+    }, [toNumber, beforeNumber])
+
+    return <Fragment>{currentNumber}</Fragment>
+  }
+
+  const isNoReRender: IsNoReRender<NumberTransitionProps> = (beforeProps, afterProps) =>
+    beforeProps.toNumber === afterProps.toNumber
+
+  return memo(NumberTransition, isNoReRender)
+}
+
+export default getNumberTransition
