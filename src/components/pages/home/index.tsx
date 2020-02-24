c2095f0c950746443594f2b51d9e9325b9b39011
Shogo Akimoto <114.s.aki@gmail.com>
2020-02-24 20:51:13 +0900

    feat(Home): add charts

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -11,16 +11,31 @@ import ContentWrapper from "../../molecules/contentWrapper"
 import CustomHead from "../../molecules/customHead"
 import PageAbove from "../../molecules/pageAbove"
 import MainContentWrapper from "../../molecules/mainContentWrapper"
+import HorizonCardWrapper from "./components/horizonCardWrapper"
+import MonthlyCard from "./components/monthlyCard"
+import PrefCard from "./components/prefCard"
+import TagCard from "./components/tagCard"
 
 /**
  * main
  */
 const Home: NextPage = () => {
+  const startMonth = 72
+  const endMonth = 1
+  const isIgnoreBlank = true
+
   return (
     <ContentWrapper>
       <CustomHead title="Home" />
       <PageAbove title="Home" />
-      <MainContentWrapper />
+      <MainContentWrapper>
+        <HorizonCardWrapper>
+          <MonthlyCard startMonth={startMonth} endMonth={endMonth} isIgnoreBlank={isIgnoreBlank} />
+          <PrefCard startMonth={startMonth} endMonth={endMonth} />
+        </HorizonCardWrapper>
+
+        <TagCard startMonth={startMonth} endMonth={endMonth} />
+      </MainContentWrapper>
     </ContentWrapper>
   )
 }
