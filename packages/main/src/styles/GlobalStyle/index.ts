eb985d9cb8531fbf0e36fc4ca477f7833b700aab
akky-xxxx <114.s.aki@gmail.com>
2020-11-21 17:23:24 +0900

    feat(main): define global style

---
+++ packages/main/src/styles/GlobalStyle/index.ts
@@ -0,0 +1,10 @@
+// import node_modules
+import { createGlobalStyle } from "styled-components"
+
+// import css
+import { reset } from "@@/styles/GlobalStyle/css/reset"
+
+// main
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+`
