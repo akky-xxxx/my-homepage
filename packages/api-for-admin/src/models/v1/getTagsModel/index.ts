6e5c0adccec3c3a4e555698649f55db294db8e28
akky-xxxx <114.s.aki@gmail.com>
2020-11-28 22:08:25 +0900

    feat(api-for-admin): implement model

--- packages/api-for-admin/src/models/v1/getTagsModel/index.ts
+++ packages/api-for-admin/src/models/v1/getTagsModel/index.ts
@@ -1,28 +1,29 @@
 // import
 import { GetTagsResponse } from "@@/shared/types/api/v1/tags"
 import { ThisError } from "@@/shared/utils/ThisError"
+import { dataStore } from "@@/shared/utils/gcp"
+import { DataStore } from "@@/shared/const/DataStore"
 
 // main
+const {
+  TYPES: { TAGS },
+} = DataStore
+
 type GetTagsModel = () => Promise<GetTagsResponse>
 export const getTagsModel: GetTagsModel = async () => {
-  const responseData: GetTagsResponse = {
-    data: {
-      tags: [
-        {
-          tagId: "1",
-          tagName: "tag1",
-          isRelease: true,
-          createdAt: "2020-01-01",
-          updatedAt: "2020-01-01",
-        },
-      ],
-    },
-  }
+  const query = dataStore.createQuery(TAGS)
 
   try {
+    const [tags] = await dataStore.runQuery(query)
+    const responseData: GetTagsResponse = {
+      data: {
+        tags,
+      },
+    }
+
     return Promise.resolve(responseData)
   } catch (error) {
-    const thisError = new ThisError({ error })
+    const thisError = new ThisError({ error, filePath: __filename })
     return Promise.reject(thisError)
   }
 }
