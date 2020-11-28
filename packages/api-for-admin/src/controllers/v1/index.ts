417ce80656c963c69f3fb556296c277598a0aa75
akky-xxxx <114.s.aki@gmail.com>
2020-11-29 00:27:02 +0900

    feat(api-for-admin): implement temporary request handler

--- packages/api-for-admin/src/controllers/v1/index.ts
+++ packages/api-for-admin/src/controllers/v1/index.ts
@@ -6,6 +6,7 @@ import { getTagsController } from "@@/controllers/v1/getTagsController"
 
 // import others
 import { Endpoints } from "@@/shared/const/Endpoints"
+import { ThisError } from "@@/shared/utils/ThisError"
 
 // main
 const {
@@ -13,6 +14,31 @@ const {
 } = Endpoints
 const v1Router = Router()
 
+v1Router.post(TAGS, (req, res, next) => {
+  const { body } = req
+
+  if (!body.tagName) {
+    const error = new ThisError({
+      status: 400,
+      message: "パラメータが不正です",
+      filePath: __filename,
+    })
+    next(error)
+    return
+  }
+
+  try {
+    const responseData = {
+      data: {
+        result: "success",
+      },
+    }
+    res.status(201).send(responseData)
+  } catch (error) {
+    const thisError = new ThisError({ error })
+    next(thisError)
+  }
+})
 v1Router.get(TAGS, getTagsController)
 
 export { v1Router }
