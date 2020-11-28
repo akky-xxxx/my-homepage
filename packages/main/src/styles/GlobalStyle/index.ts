e45a7ebb581d32b2e3760fa4031c6a630c4001b9
akky-xxxx <114.s.aki@gmail.com>
2020-11-28 13:40:15 +0900

    feat(main): use common style

--- packages/main/src/styles/GlobalStyle/index.ts
+++ packages/main/src/styles/GlobalStyle/index.ts
@@ -3,8 +3,10 @@ import { createGlobalStyle } from "styled-components"
 
 // import css
 import { reset } from "@@/styles/GlobalStyle/css/reset"
+import { common } from "@@/styles/GlobalStyle/css/common"
 
 // main
 export const GlobalStyle = createGlobalStyle`
   ${reset}
+  ${common}
 `
