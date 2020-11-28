858679ae286c7baae9cc821b74e4f99ddb312df8
akky-xxxx <114.s.aki@gmail.com>
2020-11-28 21:28:31 +0900

    feat(api-for-admin): define temporary request handler

--- packages/api-for-admin/src/controllers/v1/index.ts
+++ packages/api-for-admin/src/controllers/v1/index.ts
@@ -6,6 +6,7 @@ import { sampleController } from "@@/controllers/v1/sampleController"
 
 // import
 import { Endpoints } from "@@/shared/const/Endpoints"
+import { Tag } from "@@/shared/types/api/v1/tags"
 
 // main
 const {
@@ -14,5 +15,15 @@ const {
 const v1Router = Router()
 
 v1Router.get(SAMPLE, sampleController)
+v1Router.get("/tags", (_req, res) => {
+  const tag: Tag = {
+    tagId: "1",
+    tagName: "tag1",
+    isRelease: true,
+    createdAt: "2020-01-01",
+    updatedAt: "2020-01-01",
+  }
+  res.status(200).send({ data: { tags: [tag] } })
+})
 
 export { v1Router }
