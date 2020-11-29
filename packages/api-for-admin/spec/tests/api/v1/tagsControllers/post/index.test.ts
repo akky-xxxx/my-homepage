da517aa476c42b4c350f004a4f6caaec9108c660
akky-xxxx <114.s.aki@gmail.com>
2020-11-29 14:04:20 +0900

    test(api-for-admin): use module

--- packages/api-for-admin/spec/tests/api/v1/tagsControllers/post/index.test.ts
+++ packages/api-for-admin/spec/tests/api/v1/tagsControllers/post/index.test.ts
@@ -4,6 +4,7 @@ import supertest from "supertest"
 // import others
 import { server } from "@@/modules/server"
 import { PostTagsResponse } from "@@/shared/types/api/v1/tags"
+import { removeTestRecords, TAG_NAME } from "../modules/dataStore"
 
 // main
 const responseData: PostTagsResponse["data"] = {
@@ -14,25 +15,27 @@ describe("app test `POST:/api/v1/tags`", () => {
   it("return 201 and correct property when tagName is string", async () => {
     await request
       .post("/api/v1/tags")
-      .send({ tagNames: "test" })
+      .send({ tagNames: TAG_NAME })
       .expect(201)
       .then((res) => {
         const { data } = res.body
         expect(typeof data).toEqual("object")
         expect(data).toEqual(responseData)
       })
+    await removeTestRecords()
   })
 
   it("return 201 and correct property when tagName is array of string", async () => {
     await request
       .post("/api/v1/tags")
-      .send({ tagNames: ["test", `test`] })
+      .send({ tagNames: [TAG_NAME, TAG_NAME] })
       .expect(201)
       .then((res) => {
         const { data } = res.body
         expect(typeof data).toEqual("object")
         expect(data).toEqual(responseData)
       })
+    await removeTestRecords()
   })
 
   it("return 400 when not have request body", async () => {
