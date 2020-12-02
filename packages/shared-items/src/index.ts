5032eaa84be569a1859d9a4547b3e8596a009146
akky-xxxx <114.s.aki@gmail.com>
2020-12-03 01:14:29 +0900

    refactor(shared-items): change structure

--- packages/shared-items/src/index.ts
+++ packages/shared-items/src/index.ts
@@ -1,37 +1,18 @@
-/**
- * @typedef EmptyFunction
- * @description 空関数
- */
-export type EmptyFunction = () => void
+// import common
+import {
+  EmptyFunction,
+  AnyFunction,
+  EmptyObject,
+  AnyObject,
+  Nullable,
+  StringDatetime,
+} from "./common/types"
 
-/**
- * @typedef AnyFunction
- * @description Function の代わりに使用
- */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type AnyFunction = (...args: any) => any
-
-/**
- * @typedef EmptyObject
- * @description {} の代わりに使用
- */
-export type EmptyObject = Record<never, never>
-
-/**
- * @typedef AnyObject
- * @description object の代わりに使用
- */
-export type AnyObject = Record<string, unknown>
-
-/**
- * @typedef Nullable
- * @description nullable な型を定義
- * @param T - null でない時の型
- */
-export type Nullable<T> = T | null
-
-/**
- * @typedef StringDatetime
- * @description 日付形式を指す type alias
- */
-export type StringDatetime = string
+export type {
+  EmptyFunction,
+  AnyFunction,
+  EmptyObject,
+  AnyObject,
+  Nullable,
+  StringDatetime,
+}
