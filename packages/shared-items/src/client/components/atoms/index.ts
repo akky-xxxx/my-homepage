3f370bca59ecddfa545f6a6e4f11712db50b5f24
akky-xxxx <114.s.aki@gmail.com>
2020-12-03 23:39:43 +0900

    feat(shared-items): add export the components

---
+++ packages/shared-items/src/client/components/atoms/index.ts
@@ -0,0 +1,7 @@
+// import
+export { Button } from "./Button"
+export type { ButtonProps } from "./Button/types"
+export { MenuButton } from "./MenuButton"
+export type { MenuButtonProps } from "./MenuButton/types"
+export { Tag } from "./Tag"
+export type { TagProps } from "./Tag/types"
--- packages/shared-items/src/index.ts
+++ packages/shared-items/src/index.ts
@@ -1,2 +1,5 @@
 // import common
 export * from "./common"
+
+// client
+export * from "./client"
