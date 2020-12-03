e733991ecb94edfb652d102486ad5f5dcaf08224
akky-xxxx <114.s.aki@gmail.com>
2020-12-03 22:20:44 +0900

    refactor(shared-items): change export method

--- packages/shared-items/src/index.ts
+++ packages/shared-items/src/index.ts
@@ -1,18 +1,2 @@
 // import common
-import {
-  EmptyFunction,
-  AnyFunction,
-  EmptyObject,
-  AnyObject,
-  Nullable,
-  StringDatetime,
-} from "./common/types"
-
-export type {
-  EmptyFunction,
-  AnyFunction,
-  EmptyObject,
-  AnyObject,
-  Nullable,
-  StringDatetime,
-}
+export * from "./common"
