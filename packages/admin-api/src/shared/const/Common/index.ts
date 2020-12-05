05548fae316884198c82ef34dfe92e2878bd6627
akky-xxxx <114.s.aki@gmail.com>
2020-12-05 21:51:08 +0900

    refactor(admin-api): remove SEPARATOR

--- packages/admin-api/src/shared/const/Common/index.ts
+++ packages/admin-api/src/shared/const/Common/index.ts
@@ -2,6 +2,4 @@
 export const Common = {
   IS_DEV: process.env.NODE_ENV !== "production",
   IS_TEST: process.env.IS_TEST !== "true",
-  SEPARATOR:
-    "//////////////////////////////////////////////////////////////////////",
 } as const
