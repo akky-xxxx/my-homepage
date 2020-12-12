7128adf9237a1ffd92d463099a1f6667d2ecff42
akky-xxxx <114.s.aki@gmail.com>
2020-12-12 15:24:47 +0900

    refactor(admin): change class component to functional component

--- packages/admin/src/pages/_app.tsx
+++ packages/admin/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 // import node_modules
-import App, { AppInitialProps } from "next/app"
+import { AppProps } from "next/app"
 import Head from "next/head"
-import React from "react"
+import React, { FC } from "react"
 import { GlobalStyle } from "shared-items/dist/client"
 import { Provider } from "react-redux"
 
@@ -9,24 +9,22 @@ import { Provider } from "react-redux"
 import { store } from "@@/store"
 
 // main
-class MyApp extends App<AppInitialProps> {
-  render() {
-    const { Component, pageProps } = this.props
+const MyApp: FC<AppProps> = (props) => {
+  const { Component, pageProps } = props
 
-    return (
-      <Provider store={store}>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, minimum-scale=1.0"
-          />
-          <title>fastify site with next.js</title>
-        </Head>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </Provider>
-    )
-  }
+  return (
+    <Provider store={store}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0"
+        />
+        <title>fastify site with next.js</title>
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
