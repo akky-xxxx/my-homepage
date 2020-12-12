fdc3313a83486f52b5d83cb551b3d789baea40f1
akky-xxxx <114.s.aki@gmail.com>
2020-12-12 15:41:49 +0900

    feat(admin): verification implement of getServerSideProps

--- packages/admin/src/pages/sample/index.tsx
+++ packages/admin/src/pages/sample/index.tsx
@@ -6,11 +6,25 @@ import { Sample } from "@@/components/templates/Sample"
 
 // import others
 import { useSampleHooks } from "@@/pageHooks/useSampleHooks"
+import { wrapper, actions } from "@@/store"
 
 // main
+const {
+  sample: { changeText },
+} = actions
 const HomeHoc = () => {
   const props = useSampleHooks()
   return <Sample {...props} />
 }
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  async (context) => {
+    const {
+      store: { dispatch },
+    } = context
+
+    await dispatch(changeText({ newText: "getting at getServerSideProps" }))
+  },
+)
 
 export default HomeHoc
