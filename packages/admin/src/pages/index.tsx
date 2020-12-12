353a8f3b9eb6afc5c49c2b896b49dc529644344a
akky-xxxx <114.s.aki@gmail.com>
2020-12-12 16:01:28 +0900

    feat(admin): implement temporary top

---
+++ packages/admin/src/pages/index.tsx
@@ -0,0 +1,15 @@
+// import node_modules
+import React from "react"
+import Link from "next/link"
+
+// main
+const Home = () => (
+  <div>
+    <h1>home</h1>
+    <div>
+      <Link href="/sample">サンプル</Link>
+    </div>
+  </div>
+)
+
+export default Home
