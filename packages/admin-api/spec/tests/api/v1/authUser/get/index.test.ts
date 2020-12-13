278cfa0ec0280afaa65dd0fbd9e7fcacbb2f4be6
akky-xxxx <114.s.aki@gmail.com>
2020-12-13 16:12:39 +0900

    test(admin-api): add check property of env

--- packages/admin-api/spec/tests/api/v1/authUser/get/index.test.ts
+++ packages/admin-api/spec/tests/api/v1/authUser/get/index.test.ts
@@ -1,6 +1,7 @@
 // import node_modules
 import supertest from "supertest"
 import dotenv from "dotenv"
+import { ThisError } from "shared-items"
 
 // import others
 import { server } from "@@/modules/server"
@@ -9,6 +10,13 @@ import { server } from "@@/modules/server"
 dotenv.config()
 
 const googleId = process.env.TEST_PERMISSION_USER
+
+if (!googleId) {
+  throw new ThisError({
+    filePath: __filename,
+    message: "TEST_PERMISSION_USER がありません"
+  })
+}
 
 const request = supertest(server)
 describe("app test `GET:/api/v1/auth/user`", () => {
