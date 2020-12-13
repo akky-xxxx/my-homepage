265b87fb0279d11b439ca080e7dc7589e0213f0d
akky-xxxx <114.s.aki@gmail.com>
2020-12-13 18:31:12 +0900

    feat(admin-api): add pattern of api mock

--- packages/admin-api/spec/agreed/api/v1/authUser/get/index.ts
+++ packages/admin-api/spec/agreed/api/v1/authUser/get/index.ts
@@ -41,13 +41,33 @@ export const get: ApiType[] = [
   },
   {
     title: "ユーザ認証：権限チェック",
-    description: "正常 - 権限あり",
+    description: "正常 - google id あり",
     request: {
       path: ["api", "v1", "auth", "user"],
       method: "GET",
       body: undefined,
       query: {
         googleId: "{:googleId}",
+      },
+    },
+    response: {
+      status: 200,
+      body: {
+        data: {
+          result: "success",
+        },
+      },
+    },
+  },
+  {
+    title: "ユーザ認証：権限チェック",
+    description: "正常 - session id あり",
+    request: {
+      path: ["api", "v1", "auth", "user"],
+      method: "GET",
+      body: undefined,
+      query: {
+        sessionId: "{:sessionId}",
       },
     },
     response: {
