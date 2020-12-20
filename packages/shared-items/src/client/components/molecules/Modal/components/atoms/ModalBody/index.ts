2364221f4259bee480a6c7968a6d89f78ca1a85f
akky-xxxx <114.s.aki@gmail.com>
2020-12-20 20:03:46 +0900

    refactor(shared-items): add flex-glow

--- packages/shared-items/src/client/components/molecules/Modal/components/atoms/ModalBody/index.ts
+++ packages/shared-items/src/client/components/molecules/Modal/components/atoms/ModalBody/index.ts
@@ -7,6 +7,7 @@ import { Margin } from "../../../../../../const"
 // main
 const { MARGIN10 } = Margin
 export const ModalBody = styled.div`
+  flex-grow: 1;
   overflow: auto;
   padding: 0 ${MARGIN10}px;
 `
