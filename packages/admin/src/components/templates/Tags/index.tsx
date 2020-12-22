872238c56f290be25b999d5b850681f44dad8879
akky-xxxx <114.s.aki@gmail.com>
2020-12-23 00:02:35 +0900

    feat(admin): use TagsTable

--- packages/admin/src/components/templates/Tags/index.tsx
+++ packages/admin/src/components/templates/Tags/index.tsx
@@ -5,6 +5,7 @@ import { Divider } from "shared-items/dist/client"
 // import components
 import { MainContents } from "@@/components/atoms/MainContents"
 import { Heading2 } from "@@/components/atoms/Heading2"
+import { TagsTable } from "./components/molecules/TagsTable"
 
 // main
 export const Tags = () => (
@@ -16,7 +17,7 @@ export const Tags = () => (
     <Divider />
 
     <MainContents>
-      <h2>タグコンテンツ</h2>
+      <TagsTable />
     </MainContents>
   </div>
 )
