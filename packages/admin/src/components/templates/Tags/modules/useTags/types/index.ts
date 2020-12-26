7a2a94abb6e9b30690350fca9debf608980f20b3
akky-xxxx <114.s.aki@gmail.com>
2020-12-27 00:23:10 +0900

    style(admin): apply fix task

--- packages/admin/src/components/templates/Tags/modules/useTags/types/index.ts
+++ packages/admin/src/components/templates/Tags/modules/useTags/types/index.ts
@@ -1,9 +1,11 @@
 // import
-import {TagsProps} from "../../../types"
-import {UseTagList} from "../modules/useTagList/types"
-import {UseAdditionModal} from "../modules/useAdditionModal/types"
-import {UseChangingModal} from "../modules/useChangingModal/types"
+import { TagsProps } from "../../../types"
+import { UseTagList } from "../modules/useTagList/types"
+import { UseAdditionModal } from "../modules/useAdditionModal/types"
+import { UseChangingModal } from "../modules/useChangingModal/types"
 
 // main
-type UseTagsReturn = ReturnType<UseTagList> & ReturnType<UseAdditionModal> & ReturnType<UseChangingModal>
+type UseTagsReturn = ReturnType<UseTagList> &
+  ReturnType<UseAdditionModal> &
+  ReturnType<UseChangingModal>
 export type UseTags = (props: TagsProps) => UseTagsReturn
