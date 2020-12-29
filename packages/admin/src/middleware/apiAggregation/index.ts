7fd782e8d6abb9204d667cdb866ac8d02da47a16
akky-xxxx <114.s.aki@gmail.com>
2020-12-29 20:47:00 +0900

    feat(admin): connect controller of addTags to api aggregation

--- packages/admin/src/middleware/apiAggregation/index.ts
+++ packages/admin/src/middleware/apiAggregation/index.ts
@@ -3,10 +3,12 @@ import { Router } from "express"
 
 // import controllers
 import { updateTags } from "@@/controllers/updateTags"
+import { addTags } from "@@/controllers/addTags"
 
 // main
 const apiAggregation = Router()
 
+apiAggregation.post("/tags", addTags)
 apiAggregation.put("/tags", updateTags)
 
 export { apiAggregation }
