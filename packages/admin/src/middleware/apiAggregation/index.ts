95a2e4b5d2514ac469efeb4d22962549ad40a6e2
akky-xxxx <114.s.aki@gmail.com>
2020-12-30 21:24:19 +0900

    feat(admin): implement controller of deleteTags

--- packages/admin/src/middleware/apiAggregation/index.ts
+++ packages/admin/src/middleware/apiAggregation/index.ts
@@ -4,11 +4,13 @@ import { Router } from "express"
 // import controllers
 import { updateTags } from "@@/controllers/updateTags"
 import { addTags } from "@@/controllers/addTags"
+import { deleteTags } from "@@/controllers/deleteTags"
 
 // main
 const apiAggregation = Router()
 
 apiAggregation.post("/tags", addTags)
 apiAggregation.put("/tags", updateTags)
+apiAggregation.delete("/tags", deleteTags)
 
 export { apiAggregation }
