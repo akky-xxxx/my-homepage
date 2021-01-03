aee772c4eb8eff2be9185279b0977fc50536094c
akky-xxxx <114.s.aki@gmail.com>
2021-01-03 20:41:43 +0900

    feat(admin): use Pagination

--- packages/admin/src/components/templates/Tags/index.tsx
+++ packages/admin/src/components/templates/Tags/index.tsx
@@ -1,12 +1,13 @@
 // import node_modules
 import React, { FC } from "react"
-import { Divider } from "shared-items/dist/client"
+import { Divider, Margin } from "shared-items/dist/client"
 import styled from "styled-components"
 
 // import components
 import { MainContents } from "@@/components/atoms/MainContents"
 import { Heading2 } from "@@/components/atoms/Heading2"
 import { Loading } from "@@/components/atoms/Loading"
+import { Pagination } from "@@/components/molecules/Pagination"
 import { SubMenu } from "./components/molecules/SubMenu"
 import { TagsTable } from "./components/organisms/TagsTable"
 import { AdditionModal } from "./components/organisms/AdditionModal"
@@ -19,6 +20,8 @@ import { TagsProps } from "./types"
 import { useTags } from "./modules/useTags"
 
 // main
+const { MARGIN20 } = Margin
+
 export const Tags: FC<TagsProps> = (props) => {
   const {
     handleUpdateTagsMain,
@@ -87,6 +90,16 @@ export const Tags: FC<TagsProps> = (props) => {
           handleClickSelect={handleClickSelect}
           handleClickRelease={handleClickRelease}
         />
+
+        <PaginationWrapper>
+          <Pagination
+            maxPages={10}
+            currentPage={2}
+            // TODO: 仮実装
+            // eslint-disable-next-line no-console
+            handleClickPagination={() => console.log("handleClickPagination")}
+          />
+        </PaginationWrapper>
       </MainContents>
 
       <Loading isShow={isLoading || isTagsLoading} />
@@ -117,4 +130,9 @@ export const Tags: FC<TagsProps> = (props) => {
 const HeaderWrapper = styled.div`
   align-items: center;
   display: flex;
+`
+
+const PaginationWrapper = styled.div`
+  margin-top: ${MARGIN20}px;
+  text-align: center;
 `
