4d4a354a531ab0a8212cb735ba4587777954d306
akky-xxxx <114.s.aki@gmail.com>
2021-01-06 00:10:18 +0900

    feat(admin): use HorizonDateRangePicker

--- packages/admin/src/components/templates/Tags/components/organisms/ConditionArea/index.tsx
+++ packages/admin/src/components/templates/Tags/components/organisms/ConditionArea/index.tsx
@@ -7,6 +7,7 @@ import { Margin } from "shared-items/dist/client"
 // import components
 import { Input } from "@@/components/atoms/Input"
 import { SelectOption } from "@@/shared/types/lib"
+import { HorizonDateRangePicker } from "@@/components/templates/Tags/components/molecules/HorizonDateRangePicker"
 
 // main
 const { MARGIN20 } = Margin
@@ -31,14 +32,7 @@ export const ConditionArea: FC<ConditionAreaProps> = (props) => {
   return (
     <Fragment>
       <Wrapper>
-        <StyledInput
-          value={filterText}
-          onChange={handleChangeFilterText}
-          placeholder="部分一致検索"
-          type="text"
-        />
-
-        <Select<SelectOption, true>
+        <StyledSelect
           options={selectOptions}
           placeholder="タグを選択"
           value={selectedOptions}
@@ -49,25 +43,49 @@ export const ConditionArea: FC<ConditionAreaProps> = (props) => {
           isSearchable
         />
       </Wrapper>
+
+      <Wrapper>
+        <Input
+          value={filterText}
+          onChange={handleChangeFilterText}
+          placeholder="部分一致検索"
+          type="text"
+        />
+
+        <div>
+          追加日：
+          <HorizonDateRangePicker
+            startDate={new Date()}
+            endDate={new Date()}
+            handleChangeStartDate={() => console.log("handleChangeStartDate")}
+            handleChangeEndDate={() => console.log("handleChangeEndDate")}
+          />
+        </div>
+
+        <div>
+          変更日：
+          <HorizonDateRangePicker
+            startDate={new Date()}
+            endDate={new Date()}
+            handleChangeStartDate={() => console.log("handleChangeStartDate")}
+            handleChangeEndDate={() => console.log("handleChangeEndDate")}
+          />
+        </div>
+      </Wrapper>
     </Fragment>
   )
 }
 
 const Wrapper = styled.div`
-  align-items: flex-start;
   display: flex;
-  width: 100%;
+  justify-content: space-between;
+  width: 850px;
 
   & + & {
     margin-top: ${MARGIN20}px;
   }
-
-  & > div {
-    flex-grow: 1;
-    flex-shrink: 0;
-  }
 `
 
-const StyledInput = styled(Input)`
-  margin-right: ${MARGIN20}px;
+const StyledSelect = styled(Select)`
+  flex-grow: 1;
 `
