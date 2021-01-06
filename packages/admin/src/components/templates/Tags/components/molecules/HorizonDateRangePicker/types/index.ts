990c33ff5ec057a7b928e8f41154a67f71ea3c26
akky-xxxx <114.s.aki@gmail.com>
2021-01-06 23:49:38 +0900

    feat(admin): add property to type

--- packages/admin/src/components/templates/Tags/components/molecules/HorizonDateRangePicker/types/index.ts
+++ packages/admin/src/components/templates/Tags/components/molecules/HorizonDateRangePicker/types/index.ts
@@ -1,6 +1,7 @@
 export type HorizonDateRangePickerProps = {
   startDate: Date | null
   endDate: Date | null
+  accentDates?: Date[]
   handleChangeStartDate: (date: Date | null) => void
   handleChangeEndDate: (date: Date | null) => void
 }
