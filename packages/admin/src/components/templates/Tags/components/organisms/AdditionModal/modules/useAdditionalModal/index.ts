93074cb95996064217fd673f1bbc96aac281a85b
akky-xxxx <114.s.aki@gmail.com>
2021-01-09 23:13:25 +0900

    refactor(admin): replace to createNewStates

--- packages/admin/src/components/templates/Tags/components/organisms/AdditionModal/modules/useAdditionalModal/index.ts
+++ packages/admin/src/components/templates/Tags/components/organisms/AdditionModal/modules/useAdditionalModal/index.ts
@@ -4,6 +4,7 @@ import { useState } from "react"
 // import others
 import { UseAdditionalModal, HandleChangeNewTagName, State } from "./types"
 import { createInitialState } from "./modules/createInitialState"
+import { createNewStates } from "./modules/createNewStates"
 
 // main
 export const useAdditionalModal: UseAdditionalModal = (args) => {
@@ -18,16 +19,8 @@ export const useAdditionalModal: UseAdditionalModal = (args) => {
     const {
       currentTarget: { value },
     } = event
-    const newStates = newTagNames.reduce<State[]>(
-      (newState, newTagName, index) => {
-        if (newTagName.id === id) return [...newState, { id, value }]
-        if (newTagName.value === "") return newState
-        if (index === newTagNames.length - 1)
-          return [...newState, createInitialState()]
-        return [...newState, newTagName]
-      },
-      [],
-    )
+    const createNewStatesMain = createNewStates({ id, value })
+    const newStates = newTagNames.reduce(createNewStatesMain, [])
     const isLastFilled = newStates[newStates.length - 1].value.length > 0
     if (isLastFilled) newStates.push(createInitialState())
     setNewTagNames(newStates)
