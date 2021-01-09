1b05882aa5cb2b60d501e6dd22ac8098e8515c5c
akky-xxxx <114.s.aki@gmail.com>
2021-01-09 14:49:15 +0900

    feat(admin): use usePagination

--- packages/admin/src/components/molecules/Pagination/index.tsx
+++ packages/admin/src/components/molecules/Pagination/index.tsx
@@ -5,29 +5,22 @@ import styled from "styled-components"
 
 // import others
 import { PaginationProps } from "./types"
+import { usePagination } from "./modules/usePagination"
 
 // main
 const { MARGIN10 } = Margin
 
 export const Pagination: FC<PaginationProps> = (props) => {
   const { maxPages, currentPage, handleClickPagination } = props
-
-  const enablePrev = currentPage > 1
-  const enableNext = currentPage < maxPages
-  const handleClickFirst = () => handleClickPagination(1)
-  const handleClickPrev = () => handleClickPagination(currentPage - 1)
-  const handleClickNext = () => handleClickPagination(currentPage + 1)
-  const handleClickLast = () => handleClickPagination(maxPages)
-
-  const items = [
-    currentPage - 1 > 0 ? currentPage - 1 : undefined,
-    currentPage,
-    currentPage + 1 <= maxPages ? currentPage + 1 : undefined,
-  ].filter(Boolean) as number[]
-  if (items.length < 3) {
-    if (currentPage - 2 > 0) items.unshift(currentPage - 2)
-    if (currentPage + 2 <= maxPages) items.push(currentPage + 2)
-  }
+  const {
+    enablePrev,
+    enableNext,
+    items,
+    handleClickFirst,
+    handleClickPrev,
+    handleClickNext,
+    handleClickLast,
+  } = usePagination(props)
 
   return (
     <Wrapper>
