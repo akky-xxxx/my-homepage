bede3ae152e59e41059aac52273e1e7eaa4e10fc
akky-xxxx <114.s.aki@gmail.com>
2021-01-09 22:42:42 +0900

    refactor(admin): replace to createInitialState

--- packages/admin/src/components/templates/Tags/components/organisms/AdditionModal/modules/useAdditionalModal/index.ts
+++ packages/admin/src/components/templates/Tags/components/organisms/AdditionModal/modules/useAdditionalModal/index.ts
@@ -1,16 +1,11 @@
 // import node_modules
 import { useState } from "react"
-import { v4 as uuid } from "uuid"
 
 // import others
 import { UseAdditionalModal, HandleChangeNewTagName, State } from "./types"
+import { createInitialState } from "./modules/createInitialState"
 
 // main
-const createInitialState = (): State => ({
-  id: uuid(),
-  value: "",
-})
-
 export const useAdditionalModal: UseAdditionalModal = (args) => {
   const { handleAddTagsMain, handleHideAdditionModal } = args
   const [newTagNames, setNewTagNames] = useState<State[]>([
