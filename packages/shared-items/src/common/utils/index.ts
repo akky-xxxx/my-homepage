db0189e61693363d3e788bc7f105d2bb33855189
akky-xxxx <114.s.aki@gmail.com>
2021-01-10 23:32:14 +0900

    refactor(shared-items): move createLoggerBase to server

--- packages/shared-items/src/common/utils/index.ts
+++ packages/shared-items/src/common/utils/index.ts
@@ -1,5 +1,4 @@
 export { createErrorData } from "./createErrorData"
-export { createLoggerBase } from "./createLoggerBase"
 export { createNullArray } from "./createNullArray"
 export { removeUndefinedKeys } from "./removeUndefinedKeys"
 export { ThisError } from "./ThisError"
