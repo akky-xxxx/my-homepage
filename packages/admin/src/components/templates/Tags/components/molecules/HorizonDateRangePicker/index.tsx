18bde667e5b3742eba8a8dc1192ec9e3c1dad05d
akky-xxxx <114.s.aki@gmail.com>
2021-01-10 15:57:29 +0900

    perf(admin): use useMemo, memo

--- packages/admin/src/components/templates/Tags/components/molecules/HorizonDateRangePicker/index.tsx
+++ packages/admin/src/components/templates/Tags/components/molecules/HorizonDateRangePicker/index.tsx
@@ -1,5 +1,5 @@
 // import node_modules
-import React, { FC } from "react"
+import React, { FC, memo } from "react"
 import ReactDatePicker from "react-datepicker"
 import { PickerWrapper, Input, Radius, Color } from "shared-items/dist/client"
 import styled, { css } from "styled-components"
@@ -12,52 +12,52 @@ import { useHorizonDateRangePicker } from "./modules/useHorizonDateRangePicker"
 const { UNIT_LEFT, UNIT_RIGHT } = Radius
 const { COLOR_FFFFFF, DIVIDER } = Color
 
-export const HorizonDateRangePicker: FC<HorizonDateRangePickerProps> = (
-  props,
-) => {
-  const {
-    startDate,
-    endDate,
-    handleChangeStartDate,
-    handleChangeEndDate,
-  } = props
-  const { dayClassName } = useHorizonDateRangePicker(props)
+export const HorizonDateRangePicker: FC<HorizonDateRangePickerProps> = memo(
+  (props) => {
+    const {
+      startDate,
+      endDate,
+      handleChangeStartDate,
+      handleChangeEndDate,
+    } = props
+    const { dayClassName } = useHorizonDateRangePicker(props)
 
-  return (
-    <Wrapper>
-      <ReactDatePicker
-        selected={startDate}
-        onChange={handleChangeStartDate}
-        calendarContainer={PickerWrapper}
-        customInput={<StartInput />}
-        startDate={startDate}
-        endDate={endDate}
-        maxDate={new Date()}
-        selectsStart
-        dateFormat="yyyy/MM/dd"
-        placeholderText="from"
-        dayClassName={dayClassName}
-        isClearable
-      />
-      <Tilde>〜</Tilde>
-      <ReactDatePicker
-        selected={endDate}
-        onChange={handleChangeEndDate}
-        calendarContainer={PickerWrapper}
-        customInput={<EndInput />}
-        startDate={startDate}
-        endDate={endDate}
-        minDate={startDate}
-        maxDate={new Date()}
-        selectsEnd
-        dateFormat="yyyy/MM/dd"
-        placeholderText="to"
-        dayClassName={dayClassName}
-        isClearable
-      />
-    </Wrapper>
-  )
-}
+    return (
+      <Wrapper>
+        <ReactDatePicker
+          selected={startDate}
+          onChange={handleChangeStartDate}
+          calendarContainer={PickerWrapper}
+          customInput={<StartInput />}
+          startDate={startDate}
+          endDate={endDate}
+          maxDate={new Date()}
+          selectsStart
+          dateFormat="yyyy/MM/dd"
+          placeholderText="from"
+          dayClassName={dayClassName}
+          isClearable
+        />
+        <Tilde>〜</Tilde>
+        <ReactDatePicker
+          selected={endDate}
+          onChange={handleChangeEndDate}
+          calendarContainer={PickerWrapper}
+          customInput={<EndInput />}
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+          maxDate={new Date()}
+          selectsEnd
+          dateFormat="yyyy/MM/dd"
+          placeholderText="to"
+          dayClassName={dayClassName}
+          isClearable
+        />
+      </Wrapper>
+    )
+  },
+)
 
 export const Wrapper = styled.div`
   display: inline-flex;
