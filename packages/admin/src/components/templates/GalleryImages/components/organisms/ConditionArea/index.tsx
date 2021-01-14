4288aaa0ca1bad74ef3f71ce8a8218eb397efa99
akky-xxxx <114.s.aki@gmail.com>
2021-01-14 23:15:32 +0900

    feat(admin): add range picker of createdAt, updatedAt

--- packages/admin/src/components/templates/GalleryImages/components/organisms/ConditionArea/index.tsx
+++ packages/admin/src/components/templates/GalleryImages/components/organisms/ConditionArea/index.tsx
@@ -11,7 +11,7 @@ import { HorizonDateRangePicker } from "@@/components/molecules/HorizonDateRange
 import { ConditionAreaProps } from "./types"
 
 // main
-const { MARGIN20 } = Margin
+const { MARGIN10, MARGIN20 } = Margin
 
 export const ConditionArea: FC<ConditionAreaProps> = (props) => {
   const {
@@ -19,11 +19,19 @@ export const ConditionArea: FC<ConditionAreaProps> = (props) => {
     selectedPrefecture,
     photographAtStart,
     photographAtEnd,
+    createdAtStart,
+    createdAtEnd,
+    updatedAtStart,
+    updatedAtEnd,
     accentDates,
     tags,
     selectedTags,
     handleSelectPhotographAtStart,
     handleSelectPhotographAtEnd,
+    handleSelectCreatedAtStart,
+    handleSelectCreatedAtEnd,
+    handleSelectUpdatedAtStart,
+    handleSelectUpdatedAtEnd,
     handleSelectPrefecture,
     handleSelectTags,
     handleResetConditions,
@@ -31,35 +39,63 @@ export const ConditionArea: FC<ConditionAreaProps> = (props) => {
 
   return (
     <Wrapper>
-      <FieldWrapper>
-        <StyledSelect
-          options={prefectures}
-          value={selectedPrefecture}
-          placeholder="都道府県を選択"
-          onChange={handleSelectPrefecture}
-        />
-      </FieldWrapper>
+      <ConditionWrapper>
+        <RowWrapper>
+          <FieldWrapper>
+            <StyledSelect
+              options={prefectures}
+              value={selectedPrefecture}
+              placeholder="都道府県を選択"
+              onChange={handleSelectPrefecture}
+            />
+          </FieldWrapper>
 
-      <FieldWrapper>
-        <StyledSelect
-          options={tags}
-          value={selectedTags}
-          placeholder="タグを選択"
-          onChange={handleSelectTags}
-          isMulti
-        />
-      </FieldWrapper>
+          <FieldWrapper>
+            <StyledSelect
+              options={tags}
+              value={selectedTags}
+              placeholder="タグを選択"
+              onChange={handleSelectTags}
+              isMulti
+            />
+          </FieldWrapper>
 
-      <FieldWrapper>
-        撮影日：
-        <HorizonDateRangePicker
-          startDate={photographAtStart ? new Date(photographAtStart) : null}
-          endDate={photographAtEnd ? new Date(photographAtEnd) : null}
-          accentDates={accentDates}
-          handleChangeStartDate={handleSelectPhotographAtStart}
-          handleChangeEndDate={handleSelectPhotographAtEnd}
-        />
-      </FieldWrapper>
+          <FieldWrapper>
+            撮影日：
+            <HorizonDateRangePicker
+              startDate={photographAtStart ? new Date(photographAtStart) : null}
+              endDate={photographAtEnd ? new Date(photographAtEnd) : null}
+              accentDates={accentDates}
+              handleChangeStartDate={handleSelectPhotographAtStart}
+              handleChangeEndDate={handleSelectPhotographAtEnd}
+            />
+          </FieldWrapper>
+        </RowWrapper>
+
+        <RowWrapper>
+          <FieldWrapper>
+            登録日：
+            <HorizonDateRangePicker
+              startDate={createdAtStart ? new Date(createdAtStart) : null}
+              endDate={createdAtEnd ? new Date(createdAtEnd) : null}
+              accentDates={accentDates}
+              handleChangeStartDate={handleSelectCreatedAtStart}
+              handleChangeEndDate={handleSelectCreatedAtEnd}
+            />
+          </FieldWrapper>
+
+          <FieldWrapper>
+            変更日：
+            <HorizonDateRangePicker
+              startDate={updatedAtStart ? new Date(updatedAtStart) : null}
+              endDate={updatedAtEnd ? new Date(updatedAtEnd) : null}
+              accentDates={accentDates}
+              handleChangeStartDate={handleSelectUpdatedAtStart}
+              handleChangeEndDate={handleSelectUpdatedAtEnd}
+            />
+          </FieldWrapper>
+        </RowWrapper>
+      </ConditionWrapper>
 
       <ButtonWrapper>
         <Button colorType="accent" onClick={handleResetConditions}>
@@ -71,7 +107,21 @@ export const ConditionArea: FC<ConditionAreaProps> = (props) => {
 }
 
 const Wrapper = styled.div`
+  align-items: center;
   display: flex;
+  justify-content: space-between;
+`
+
+const ConditionWrapper = styled.div`
+  flex-grow: 1;
+`
+
+const RowWrapper = styled.div`
+  display: flex;
+
+  & + & {
+    margin-top: ${MARGIN10}px;
+  }
 `
 
 const FieldWrapper = styled.div`
