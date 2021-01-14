4c85c08d1240e49a26ab73ec348510401326a0cd
akky-xxxx <114.s.aki@gmail.com>
2021-01-14 22:26:24 +0900

    refactor(admin): bundle common type with Record

--- packages/admin/src/components/templates/GalleryImages/components/organisms/ConditionArea/types/index.ts
+++ packages/admin/src/components/templates/GalleryImages/components/organisms/ConditionArea/types/index.ts
@@ -6,25 +6,37 @@ import { ValueType } from "react-select"
 import { SelectOption } from "@@/shared/types/lib"
 
 // main
+type SelectOptions = Record<
+  "prefectures" | "tags" | "selectedTags",
+  SelectOption[]
+>
+
+type RangeValues = Record<
+  | "photographAtStart"
+  | "photographAtEnd"
+  | "updatedAtStart"
+  | "updatedAtEnd"
+  | "createdAtStart"
+  | "createdAtEnd",
+  StringDate | null
+>
+
+type RangeHandlers = Record<
+  | "handleSelectPhotographAtStart"
+  | "handleSelectPhotographAtEnd"
+  | "handleSelectCreatedAtStart"
+  | "handleSelectCreatedAtEnd"
+  | "handleSelectUpdatedAtStart"
+  | "handleSelectUpdatedAtEnd",
+  (date: Date | null) => void
+>
+
 export type ConditionAreaProps = {
-  prefectures: SelectOption[]
   selectedPrefecture: SelectOption | null
   accentDates: StringDate[]
-  tags: SelectOption[]
-  selectedTags: SelectOption[]
-  photographAtStart: StringDate | null
-  photographAtEnd: StringDate | null
-  updatedAtStart: StringDate | null
-  updatedAtEnd: StringDate | null
-  createdAtStart: StringDate | null
-  createdAtEnd: StringDate | null
-  handleSelectPhotographAtStart: (date: Date | null) => void
-  handleSelectPhotographAtEnd: (date: Date | null) => void
-  handleSelectCreatedAtStart: (date: Date | null) => void
-  handleSelectCreatedAtEnd: (date: Date | null) => void
-  handleSelectUpdatedAtStart: (date: Date | null) => void
-  handleSelectUpdatedAtEnd: (date: Date | null) => void
   handleSelectPrefecture: (prefecture: ValueType<SelectOption, false>) => void
   handleSelectTags: (tags: ValueType<SelectOption, true>) => void
   handleResetConditions: EmptyFunction
-}
+} & SelectOptions &
+  RangeValues &
+  RangeHandlers
